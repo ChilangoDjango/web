642335d9a6c5350eaee45b787ec06b2d490a83d0
Pablo Angel Trinidad Paz <pabloatp0204@gmail.com>
2015-05-14 01:45:10 -0500

    Front Page with counter

--- home/views.py
+++ home/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class CountDownView(TemplateView):
 	template_name = 'count_down.html'
+
+	def get_context_data(self):
+		context = super(CountDownView, self).get_context_data()
+		context['title'] = 'Chilango Django'
+		context['meta_description'] = 'Comunidad de desarrolladores Python y Django en la Ciudad de Mexico'
+		return context
